29e07dc8f0e5fef7f1f742c3fc9bf15a8295bf8e
Iván Salazar <ivangio.salazar@gmail.com>
2020-07-24 03:04:04 -0500

    Fix image show

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function loadImage(id, targetId) {
+	// console.log(id);
+	var el = document.getElementById(id);
+	var targetEl = targetId ? document.getElementById(targetId) : el;
+	var imageToLoad;
+
+	if (el.dataset.image) {
+		imageToLoad = el.dataset.image;
+	} else if (typeof el.currentSrc === 'undefined') {
+		imageToLoad = el.src;
+	} else {
+		imageToLoad = el.currentSrc;
+	}
+
+	if (imageToLoad) {
+		var img = new Image();
+		img.src = imageToLoad;
+		img.onload = function() {
+			console.log(img.src);
+			targetEl.classList.add('is-loaded');
+		};
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	loadImage('pictureImage', 'picture');
+	loadImage('wallpaper');
+});
