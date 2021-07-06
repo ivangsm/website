64903fd41b39197c1966b3feb8663c17d826a835
Iván Salazar <ivangio.salazar@gmail.com>
2021-07-06 01:50:46 -0500

    feat: improve js code

--- js/main.js
+++ js/main.js
@@ -1,35 +1,28 @@
 "use strict";
 
-function loadImage(id, targetId) {
-	// console.log(id);
-	const el = document.getElementById(id);
-	const targetEl = targetId ? document.getElementById(targetId) : el;
-	let imageToLoad;
+const loadImage = (id, targetId) => {
+	const elt = document.querySelector(`#${id}`)
+	const targetElt = targetId ? document.querySelector(`#${targetId}`) : elt
 
-	if (el.dataset.image) {
-		imageToLoad = el.dataset.image;
-	} else if (typeof el.currentSrc === 'undefined') {
-		imageToLoad = el.src;
-	} else {
-		imageToLoad = el.currentSrc;
-	}
+	const imageToLoad = elt.dataset.image
+		? elt.dataset.image
+		: typeof elt.currentSrc === 'undefined'
+			? elt.src
+			: elt.currentSrc
 
 	if (imageToLoad) {
-		const img = new Image();
-		img.src = imageToLoad;
-		img.onload = function () {
-			targetEl.classList.add('is-loaded');
-		};
+		const img = new window.Image()
+		img.src = imageToLoad
+		img.onload = _ => targetElt.classList.add('is-loaded')
 	}
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    loadImage('pictureImage', 'picture');
-	loadImage('wallpaper');
+document.addEventListener('DOMContentLoaded', _ => {
+	loadImage('pictureImage', 'picture')
+	loadImage('wallpaper')
 
 	const firstTime = localStorage.getItem('firstTime?');
 	const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-
 
 	if (firstTime === null && !isFirefox) {
 		localStorage.setItem('firstTime?', 0);
